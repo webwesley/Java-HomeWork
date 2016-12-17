c391ceaae4a3bf97ae773c2561657e15fcba4c3a
waford <webwesley@fordcochrane.com>
2016-12-17 18:35:24 -0500

    12/17

--- Java for School/src/iPodLab/Ipod.java
+++ Java for School/src/iPodLab/Ipod.java
@@ -1,4 +1,3 @@
-
 package iPodLab;
 
 import java.util.Random;
@@ -13,19 +12,83 @@ public class Ipod {
 
 	public Ipod() {
 
-		// String[] generas = {"Un-Listenible", "Jesus Tracks", "Roll and Rock",
-		// "Other"};
-		// for(int i = 0; i < generas.length; i++){
+		Song SampledToObbbllliiivvviiiiooonnnn = new Song("SampledToObbbllliiivvviiiiooonnnn", "Noah Hellman",
+				"The essential Noah hellman", 130);
+		Song AesteticSong = new Song("Aestetic Song", "Noah Hellman", "The essential Noah Hellman", 62);
+		Song JazzAestetic = new Song("Jazz Aestetic", "Noah Hellman", "The essential Noah Hellman", 204);
+		Song BadHombres = new Song("Bad Hombres", "Noah Hellman", "The essential Noah Hellman", 75);
+		Song NoahHellYes = new Song("Noah HellYes", "Noah Hellman", "The Essential Noah Hellman", 56);
+		Song HowIsDaWeather = new Song("How Is Da Weather", "Noah Hellman", "The essential Noah Hellman", 56);
+		Song TooMuchSausageFattner = new Song("Too much sausage fattner", "Noah Hellman", "The essential Noah Hellman",
+				190);
+		Song WhyDoesEveryTrapSongHaveChantsThisIsWhy = new Song("Why Does Every Trap Song Have Chants? This Is Why",
+				"Noah hellman", "The essential Noah Hellman", 182);
+		Song CaravanIsh = new Song("Caravan- Ish", "Noah Hellman", "The essential Noah Hellman", 205);
+		Song AndWhatIsAllepo = new Song("And What Is Allepo?", "Noah Hellman", "The essential Noah Hellman", 119);
+		Song OhhhhhYes = new Song("Ohhhhh Yes", "Noah Hellman", "The essential Noah Hellman", 228);
+		Song KPopSong1 = new Song("KPop Song 1", "Noah Hellman", "The essential Noah Hellman", 228);
+		Song Milano = new Song("Milano", "Noah Hellman", "The essential Noah Hellman", 73);
+		Song PredictionForDaddyHeGotATesla = new Song("Prediction For Daddy, He got a Tesla", "Noah Hellman",
+				"The essential Noah Hellman", 25);
+		Song DontRemixMeNoah = new Song("Don't Remix Me Noah", "Noah hellman", "The essential Noah Hellman", 231);
+		Song OkayNoProblem = new Song("Okay, No Problem", "Noah Hellman", "The essential Noah hellman", 206);
+		Song AlwaysAway = new Song("Always Away", "Noah Hellman", "The essential Noah Hellman", 169);
+		Song ConversionsWithMiles = new Song("Conversations With Miles", "Noah Hellman", "The essential Noah Hellman",
+				146);
+		Song SomeDayMyPrinceWillCome = new Song("Some Day My Prince Will Come", "Noah Hellman",
+				"The essential Noah Hellman", 297);
+		Song SundayJamsPart5 = new Song("Sunday Jams Part 5", "Noah Hellman", "The essential Noah hellman", 180);
+		Song SundayJamsPart4 = new Song("Sunday Jams Part 4", "Noah Hellman", "The essential Noah Hellman", 181);
+		Song SundayJamsPart3 = new Song("Sunday Jams Part 3", "Noah Hellman", "The essential Noah Hellman", 132);
+		Song SundayJams = new Song("Sunday Jams", "Noah Hellman", "The essential Noah Hellman", 134);
+		Song MessingAround = new Song("Messing Around", "Noah Hellman", "The essential Noah Hellman", 499);
+		Song Serendipity = new Song("Serendipity", "Noah Hellman", "The essential Noah Hellman", 331);
 
-		Song song = new Song();
-		Song jazzAesthetic = new Song("Jazz Aesthetic", "Noah Hellman", "The Essential Noah Hellman", 204);
-		Song[] unListenible = { jazzAesthetic };
+		Song InTheseLastDays = new Song("In These Last Days", "Mike Helms", "I Love Jesus", 204);
+		Song HeIsOurGod = new Song("He Is Our God", "Mike Helms", "I Love Jesus", 181);
+		Song JesusIsNotReligious = new Song("Jesus Is Not Religious", "Satan", "I LOve Jesus", 89);
+		Song WoeBabylon = new Song("Woe Babylon", "The People of Babylon", "I Love Jesus", 124);
+		Song JesusTakeTheWheel = new Song("Jesus Take The Wheel", "Noah", "I Love Jesus", 200);
+		Song JesusOnMyMind = new Song("Jesus On My Mind", "Adam and Eve", "I Love Jesus", 160);
+		Song PSALM19 = new Song("PSALM19", "Pastor Pat", "I love Jesus", 101);
+		Song WheresThePowerLord = new Song("Wheres' The Power Lord", "God", "I Love Jesus", 77);
+		Song SpirtWords = new Song("Spirit Words", "Abraham", "I Love Jesus", 60);
+		Song AllYouReallyNeedIsJesus = new Song("All You Really Need Is Jesus", "Abel", "I Love Jesus", 134);
+		Song WhatEverHappenedToJesus = new Song("What Ever Happened To Jesus", "Job", "I Love Jesus", 204);
+		Song ToChristian = new Song("To Christian", "King Saul", "I Love Jesus", 174);
+		Song JesusISMyBestFriend = new Song("Jesus Is My Best Friend", "Gabriel", "I Love Jesus", 301);
+		Song JesusBuiltMyHotrod = new Song("Jesus Built My Hot Rod", "Ruth", "I Love Jesus", 156);
+		Song Jesus = new Song("Jesus", "Queen", "I Love Jesus", 244);
 
-		Song[] jesusTracks = {};
+		Song HellToHighWay = new Song("Hell To Highway", "CDCA", "Backwards Roll", 666);
+		Song TNT = new Song("TNT", "CDAC", "Backwards Roll", 132);
+		Song RhapsodyBohemian = new Song("Rhapsody Bohemian", "neeuq", "Backwards Roll", 145);
+		Song UpYouGiveGonnaNever = new Song("Up You Give Gonna Never", "Astley Kicr", "Backwards Roll", 86);
+		Song BlackItPaint = new Song("Black It Paint", "Stationary Stones", "Bakwards Roll", 100);
+		Song HeavenToStairway = new Song("Heaven To Stairway", "Heallium submarine", "Backwards Roll", 301);
+		Song WaterTheOnSmoke = new Song("Water The On Smoke", "Shallow yellow", "Backwards Roll", 142);
+		Song NothingForMoney = new Song("Nothing For Money", "relaxing tributaries", "Backwards Roll", 201);
 
-		for (int i = 0; i < 50; i++) {
-			unListenible[i] = songs[0][i];
-		}
+		Song ThomasTheTankEngingeFtBiggieSmalls = new Song("Thomas The Tank Enginge Ft. Biggie Smalls", "Mr. Conductor",
+				"Lit music", 77);
+		Song MomsSpaghetti = new Song("moms Spaghetti", "Jake greens random crap", "Lit Music", 99);
+		Song JamesHowardChristmasClassic = new Song("James Howard Christmas Classic", "James Howard ft. Clash Royal",
+				"Lit music", 122);
+
+		Song[] UnListenible = { SampledToObbbllliiivvviiiiooonnnn, AesteticSong, JazzAestetic, BadHombres, NoahHellYes,
+				HowIsDaWeather, TooMuchSausageFattner, WhyDoesEveryTrapSongHaveChantsThisIsWhy, CaravanIsh,
+				AndWhatIsAllepo, OhhhhhYes, KPopSong1, Milano, PredictionForDaddyHeGotATesla, DontRemixMeNoah,
+				OkayNoProblem, AlwaysAway, ConversionsWithMiles, SomeDayMyPrinceWillCome, SundayJamsPart5,
+				SundayJamsPart4, SundayJamsPart3, SundayJams, MessingAround, Serendipity };
+
+		Song[] JesusTracks = { InTheseLastDays, HeIsOurGod, JesusIsNotReligious, WoeBabylon, JesusTakeTheWheel,
+				JesusOnMyMind, PSALM19, WheresThePowerLord, SpirtWords, AllYouReallyNeedIsJesus,
+				WhatEverHappenedToJesus, ToChristian, JesusISMyBestFriend, JesusBuiltMyHotrod, Jesus };
+
+		Song[] RollandRock = { HellToHighWay, TNT, RhapsodyBohemian, UpYouGiveGonnaNever, BlackItPaint,
+				HeavenToStairway, WaterTheOnSmoke, NothingForMoney };
+
+		Song[] Other = { ThomasTheTankEngingeFtBiggieSmalls, MomsSpaghetti, JamesHowardChristmasClassic };
 	}
 
 	public void printGenera() {
@@ -226,4 +289,3 @@ public class Ipod {
 		}
 	}
 }
->>>>>>> branch 'master' of https://github.com/webwesley/Java-HomeWork.git
