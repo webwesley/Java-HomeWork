ac1a5b94942f302e227a20e0ae5541471637570c
waford <webwesley@fordcochrane.com>
2016-12-23 23:12:29 -0500

    12/23

--- Java for School/src/iPodLab/iPodLabTester.java
+++ Java for School/src/iPodLab/iPodLabTester.java
@@ -5,8 +5,48 @@ public class iPodLabTester {
 	public static void main(String args[]){
 		Ipod iPod = new Ipod();
 		
-		iPod.playRandom();
+		iPod.printGenera();
+		addSpace();
+		randomize(iPod);
+		//clear();
+		iPod.printPopular();
+		//clear();
+		iPod.playSong("Bad Hombres");
+		//clear();
+		iPod.printArtist("Noah Hellman");
+		//clear();
+		iPod.printLongest();
+		//clear();
+		iPod.printShortest();
+		//clear();
+		iPod.randomSong();
+		//clear();
+		iPod.addSong("4:33", "John Cage", "Swanky Silence", 277, "Other");
+		//clear();
+		iPod.printAllSongs();
+		iPod.deleteSong("Bad Hombres");
+		iPod.printAllSongs();
+		//clear();
+//		iPod.printAlbum();
 		
+		
+	}
+	
+	private static void addSpace(){
+		System.out.println();
+		System.out.println();
+	}
+	
+	private static void clear(){
+		for(int i = 0; i <15; i++){
+			System.out.println();
+		}
+	}
+	
+	private static void randomize(Ipod iPod){
+		for(int i = 0; i < 10000; i++){
+			iPod.randomSong();
+		}
 	}
 
 }
