c68ddcd545975c49fce9c02c115291d0c07fe370
waford <webwesley@fordcochrane.com>
2017-01-03 22:58:24 -0500

    1/3 home

--- Java for School/src/bigInt/addInts.java
+++ Java for School/src/bigInt/addInts.java
@@ -13,11 +13,10 @@ public class addInts {
 	// Precondition: the sum fits into N digits.
 
 	private static int[] add(int[] a, int[] b) {
-		int[] retVal = new int[N];
 		long haha = toLong(a);
 		long shortcut = toLong(b);
 		long sum = haha + shortcut;
-		return(toArray(sum));
+		return (toArray(sum));
 	}
 
 	private static long toLong(int[] tmp) {
@@ -31,12 +30,10 @@ public class addInts {
 	private static int[] toArray(long sum) {
 		String tmp = Long.toString(sum);
 		int[] retVal = new int[N];
-		for (int i = 0; i < N; i++){
-		retVal[i] = Integer.parseInt(tmp);
+		for (int i = 0; i < N; i++) {
+			retVal[i] = Integer.parseInt(tmp);
 		}
-		
+		return retVal;
+
+	}
 }
-	
-	
-	
-	
